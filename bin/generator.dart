7050b008b70cf5eeaffc29ecc3c9b96369917326
adam <financeCoding@gmail.com>
2013-01-25 12:54:41 -0800

    Adding support for auth and no auth calls. Fixed posting with HttpClient instead of http.Client

--- bin/generator.dart
+++ bin/generator.dart
@@ -1062,35 +1062,78 @@ abstract class ConsoleClient extends Client {
     var url = new oauth2.UrlPattern(path).generate(urlParams, queryParams);
 
     Future clientCallback(http.Client client) {
-      var clientCompleter = new Completer();
+      // A dummy completer is used for the 'withClient' method, this should
+      // go away after refactoring withClient in oauth2 package
+      var clientDummyCompleter = new Completer();
+
       if (method.toLowerCase() == "get") {
         client.get(url).then((http.Response response) {
           var data = JSON.parse(response.body);
           completer.complete(data);
+          clientDummyCompleter.complete(null);          
         }, onError: (AsyncError error) {
-          clientCompleter.completeError(new APIRequestException("onError: \$error"));
           completer.completeError(new APIRequestException("onError: \$error"));
         });
 
       } else if (method.toLowerCase() == "post") {
-        client.post(url, fields: JSON.parse(body)).then((http.Response response) {
-          var data = JSON.parse(response.body);
-          completer.complete(data);
-        }, onError: (AsyncError error) {
-          clientCompleter.completeError(new APIRequestException("onError: \$error"));
-          completer.completeError(new APIRequestException("onError: \$error"));
-        });
+        // Workaround since http.Client does not properly support post for google apis
+        var postHttpClient = new HttpClient();
+        HttpClientConnection postConnection = postHttpClient.postUrl(new Uri.fromString(url));
+
+        // On connection request set the content type and key if available.
+        postConnection.onRequest = (HttpClientRequest request) {
+          request.headers.set(HttpHeaders.CONTENT_TYPE, contentType);
+          if (_auth != null) {
+            request.headers.set(HttpHeaders.AUTHORIZATION, "Bearer \${_auth.credentials.accessToken}");
+          }
+
+          request.outputStream.writeString(body);
+          request.outputStream.close();
+        };
+
+        // On connection response read in data from stream, on close parse as json and return.
+        postConnection.onResponse = (HttpClientResponse response) {
+          StringInputStream stream = new StringInputStream(response.inputStream);
+          StringBuffer onResponseBody = new StringBuffer();
+          stream.onData = () {
+            onResponseBody.add(stream.read());
+          };
+
+          stream.onClosed = () {
+            var data = JSON.parse(onResponseBody.toString());
+            completer.complete(data);
+            clientDummyCompleter.complete(null);
+            postHttpClient.shutdown();
+          };
+
+          // Handle stream error
+          stream.onError = (error) {
+            completer.completeError(new APIRequestException("POST stream error: \$error"));
+          };
+
+        };
+
+        // Handle post error
+        postConnection.onError = (error) {
+          completer.completeError(new APIRequestException("POST error: \$error"));
+        };
+
 
       } else {
-        clientCompleter.completeError(new APIRequestException("\$method Not implemented"));
+        // Method has not been implemented yet error
         completer.completeError(new APIRequestException("\$method Not implemented"));
       }
 
-      return clientCompleter.future;
+      return clientDummyCompleter.future;
     };
 
-    _auth.withClient(clientCallback);
-
+    if (makeAuthRequests && _auth != null) {
+      // Client wants an authenticated request.
+      _auth.withClient(clientCallback); // Should not care about the future here.
+    } else {
+      // Client wants a non authenticated request.
+      clientCallback(new http.Client()); // Should not care about the future here.
+    }
 
     return completer.future;
   }
