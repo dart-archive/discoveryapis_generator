3ce6695e6f5032ab5a4e0b43facf9a49615fbedd
adam <financeCoding@gmail.com>
2013-03-27 18:16:41 -0700

    Adding script for drone for type checking the generated packages.

--- test/run.sh
+++ test/run.sh
@@ -9,6 +9,9 @@ echo
 echo "Type Analysis, running dart_analyzer..."
 
 EXITSTATUS=0
+WARNINGS=0
+FAILURES=0
+PASSING=0
 
 ####
 # test files one at a time
@@ -16,38 +19,65 @@ EXITSTATUS=0
 for file in lib/*.dart
 do
   results=`$ANA $file 2>&1`
-  if [ -n "$results" ]; then
+  exit_code=$?
+  if [ $exit_code -eq 2 ]; then
+  	let FAILURES++
     EXITSTATUS=1
     echo "$results"
     echo "$file: FAILURE."
-  else
-    echo "$file: Passed analysis."
-  fi
+  elif [ $exit_code -eq 1 ]; then
+  	let WARNINGS++
+	echo "$results"
+    echo "$file: WARNING."
+  elif [ $exit_code -eq 0 ]; then
+  	let PASSING++
+	echo "$file: Passed analysis."
+  else 
+	echo "$file: exit code = $exit_code"
+fi
 done
 
-exit $EXITSTATUS
-
 # This application is not ready until dependencies are updated. 
-GENERATED_OUTPUT_DIR=./ci_api_test
-
+GENERATED_OUTPUT_DIR=output_drone
+rm -rf ${GENERATED_OUTPUT_DIR}
 dart bin/generate.dart --all --output ${GENERATED_OUTPUT_DIR}
 for package in ${GENERATED_OUTPUT_DIR}/*
 do
+	echo
+	echo "run dart_analyzer on $package"
 	if [ -d "$package" ]; then
 		pub_result=`pushd $package && pub install && popd`
-		cmd="$ANA --package-root $package/packages"
+		# we relax the analyzer here cause the 
+		# autogen packages have more dynamic nature to them. 
+		cmd="dart_analyzer --enable_type_checks --extended-exit-code --type-checks-for-inferred-types  --package-root $package/packages"
 		files="${package}/lib/*.dart"
 		for file in $files
 		do
-			#echo $cmd $file
 			results=`$cmd $file 2>&1`
-			if [ -n "$results" ]; then
+			exit_code=$?
+			if [ $exit_code -eq 2 ]; then
+				let FAILURES++
 			    EXITSTATUS=1
 			    echo "$results"
 			    echo "$file: FAILURE."
-			else
-			    echo "$file: Passed analysis."
+			elif [ $exit_code -eq 1 ]; then
+				let WARNINGS++
+				echo "$results"
+			    echo "$file: WARNING."
+			elif [ $exit_code -eq 0 ]; then
+				let PASSING++
+				echo "$file: Passed analysis."
+			else 
+				echo "$file: exit code = $exit_code"
 			fi
 		done
 	fi
 done
+
+echo "####################################################"
+echo "PASSING = $PASSING"
+echo "WARNINGS = $WARNINGS"
+echo "FAILURES = $FAILURES"
+echo "####################################################"
+
+exit $EXITSTATUS
