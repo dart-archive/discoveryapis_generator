174af57080dbc63a6467b9c99b89e858ac5ffc13
Kevin Moore <kevin@thinkpixellab.com>
2013-05-17 14:25:27 -0700

    hop_runner: removed unused method

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -1,7 +1,5 @@
 library hop_runner;
 
-import 'dart:async';
-import 'dart:io';
 import 'package:hop/hop.dart';
 import 'package:hop/hop_tasks.dart';
 
@@ -17,19 +15,4 @@ void main() {
                                              'tool/update.dart'
                                              ]));
   runHop();
-}
-
-void _assertKnownPath() {
-  // since there is no way to determine the path of 'this' file
-  // assume that Directory.current() is the root of the project.
-  // So check for existance of /bin/hop_runner.dart
-  final thisFile = new File('tool/hop_runner.dart');
-  assert(thisFile.existsSync());
-}
-
-Future<List<String>> _getLibs() {
-  return new Directory('lib').list()
-      .where((FileSystemEntity fse) => fse is File)
-      .map((File file) => file.path)
-      .toList();
 }
