cdad417b85cf3125827fe795175cd588add29078
Kevin Moore <github@j832.com>
2014-03-04 22:54:38 -0800

    update and lock down pubspec
    Fixed hop_runner for latest changes

--- test/harness_console.dart
+++ test/harness_console.dart
@@ -1,16 +1,10 @@
 library generator.test;
 
 import 'package:unittest/unittest.dart' as unittest;
-import 'package:unittest/vm_config.dart';
 
 import 'src/_generate.dart' as generate;
 
-main() {
-  testCore(new VMConfiguration());
-}
-
-void testCore(unittest.Configuration config) {
-  unittest.unittestConfiguration = config;
+void main() {
   unittest.groupSep = ' - ';
 
   generate.main();
--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -4,15 +4,11 @@ import 'package:hop/hop.dart';
 import 'package:hop/hop_tasks.dart';
 
 import 'util.dart';
-
 import '../test/harness_console.dart' as test_console;
 
 void main(List<String> args) {
-
-  addTask('test', createUnitTestTask(test_console.testCore,
+  addTask('test', createUnitTestTask(test_console.main,
       timeout: const Duration(seconds: 60)));
-
-  addTask('docs', createDartDocTask(['lib/generator.dart'], linkApi: true));
 
   //
   // Generate and analyze all libraries
@@ -23,11 +19,11 @@ void main(List<String> args) {
   //
   // Analyzer
   //
-  addTask('analyze', createAnalyzerTask(
-      [
-       'lib/generator.dart',
-       'bin/generate.dart',
-       'tool/update.dart',
-       'tool/util.dart']));
+  addTask('analyze', createAnalyzerTask([
+      'lib/generator.dart',
+      'bin/generate.dart',
+      'tool/update.dart',
+      'tool/util.dart']));
+
   runHop(args);
 }
